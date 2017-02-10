8d124ce496ca0af53247a46d06b9e8258426e23e
ludobeweb <ludovic0812@gmx.fr>
2017-02-10 15:32:29 +0100

    la

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -24,9 +24,16 @@ class DefaultController extends Controller
      * @Route("/personnage/create", name="createPerso")
      */
     public function creationPersonnage(Request $request){
-    
+        
+        
+        $form = $this->createForm(PersonnageType::class);
+        
         // replace this example code with whatever you need
-        return $this->render('default/creationPersonnage.html.twig');
-
+        return $this->render('default/creationPersonnage.html.twig', array(
+            "joueur" => $request->getSession()->get("j" . strval($request->
+            getSession()->get('actuel')))
+            "formulaire" => $form->createView()
+        ));
+        
     }
 }
--- src/AppBundle/Controller/PlayersController.php
+++ src/AppBundle/Controller/PlayersController.php
@@ -12,31 +12,59 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 /**
- * Description of PlayerController
+ * Description of PlayersController
  *
- * @author ludovic-castro
+ * @author ludo
  */
-class PlayersController  extends Controller{
+class PlayersController extends Controller {
     /**
-     * @Route("/players/add", name="addPlayers")
+     * Methode qui va ajouter les joueurs en base de données
+     * A la fin du traitement on est rediriger sur le controlleur de vue
+     * afin de retourner la vue de creation de personnages
+     * 
+     * Si le joueur existe en base de donnée, on le met en session
+     * sinon on l'enregistre, et on le met en session.
+     * @Route("/players/add",name="addPlayers")
      * @Method({"POST"})
      * @param \Request $r
      */
-    public function addPlayers(Request $r){
+    public function addPlayers(Request $r) {
         $entityManager = $this->getDoctrine()->getManager();
-        // boucle sur une valeurs de 1 à 4
-        for($i=1; $i <= 4; $i++){
-            // stockage de la valeur dans la variable email
-            $email = $r->get('j'.strval($i));
-            if($email != null){
-                $joueur = new Joueur();
-                $joueur->setEmail($email);
-                $entityManager->persist($joueur);
-                $r->getSession()->set('j'.strval($i), $joueur);
-                
+        //boucle sur valeurs de 1 à 4
+        for ($i = 1; $i <= 4; $i++) {
+            //stockage de la valeur dans la variabe email
+            $email = $r->get('j' . strval($i));
+            if ($email != null) {
+                $joueurs = $this->getDoctrine()->getRepository(Joueur::class)->findByEmail($email);
+                if ($joueurs != null) {
+                    $joueur = $joueurs[0];
+//                    return new Response($joueurs[0]->getEmail());
+                } else {
+                    //si nouveau joueur
+                    $joueur = new Joueur();
+                    $joueur->setEmail($email);
+                    $entityManager->persist($joueur);
+                }
+                //mise en session du joueur
+                $r->getSession()->set('j' . strval($i), $joueur);
             }
         }
         $entityManager->flush();
-        return new Response($r->get('j1'));
+        $r->getSession()->set('actuel', 1);
+        return $this->redirectToRoute('createPerso');
+    }
+    /**
+     * Doit etre appelée par la validation de la création du personnage précédent
+     * @param Request $r
+     * @return type
+     */
+    public function switchPlayer(Request $r) {
+        $next = $r->getSession()->get('actuel') + 1;
+        if ($r->getSession()->has('j' . strval($next))) {
+            $r->getSession()->set('actuel', $next);
+            return $this->redirectToRoute('createPerso');
+        }else{
+            return $this->redirectToRoute('game');
+        }
     }
 }
